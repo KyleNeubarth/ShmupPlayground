a173b6a8d44bbf7cc63f635e4b6ca1b0124a0459
KyleNeubarth <kyle@neubarth.org>
2020-09-23 14:04:19 -0600

    init commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Rigidbody rb;
+    
+    [Range(1, 100)]
+    public float moveForce = 5;
+    public Vector3 camOffset;
+    
+    
+    public float xOff;
+    public float yOff;
+
+    private float hAxis;
+    private float vAxis;
+    
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
+
+        mainCamera.transform.position = transform.position+camOffset;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(hAxis*Time.deltaTime * moveForce,0,vAxis*Time.deltaTime * moveForce));
+    }
+}
